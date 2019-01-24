1958475e9784e92838f992f653aff1ded670de2b
Long Zhen <h8ckers@gmail.com>
2019-01-24 17:59:16 +0800

    app starting point

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,87 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import {
+  Container,
+  Heading,
+  Box,
+  Loader,
+  Notification,
+  Button,
+} from 'react-bulma-components';
+import { checkStressLevel } from './libs/stress';
+import { startGame } from './libs/fbInstant';
+import StressForm from './containers/StressForm';
+import StressResult from './containers/StressResult';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showResult: false,
+      isLoading: true,
+      hasError: false,
+      stressLevel: 0,
+    };
   }
 
+  componentDidMount() {
+    this.handleStart();
+  }
+
+  handleStart = () => {
+    startGame()
+      .then(() => {
+        this.setState({ isLoading: false });
+      })
+      .catch(() => {
+        this.setState({ isLoading: false, hasError: true });
+      });
+  };
+
+  handleSubmit = values => {
+    this.setState({
+      showResult: true,
+      stressLevel: checkStressLevel(values),
+    });
+  };
+
+  handleReset = () => {
+    this.setState({ showResult: false });
+  };
+
   render() {
-    return <h1>What is my stress level</h1>;
+    const { showResult, stressLevel, isLoading, hasError } = this.state;
+
+    if (isLoading) {
+      return <Loader />;
+    }
+
+    if (hasError) {
+      return (
+        <Box>
+          <Notification color="danger">
+            There was problem starting the game.
+          </Notification>
+          <Button color="primary">Click here to start again</Button>
+        </Box>
+      );
+    }
+
+    return (
+      <Container>
+        <Box>
+          <Heading size={1}>Check your stress level</Heading>
+          {showResult ? (
+            <StressResult
+              stressLevel={stressLevel}
+              onReset={this.handleReset}
+            />
+          ) : (
+            <StressForm onSubmit={this.handleSubmit} />
+          )}
+        </Box>
+      </Container>
+    );
   }
 }
 
