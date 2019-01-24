f0620ac64b350d1433c396f6f43f42509630d7e6
Long Zhen <h8ckers@gmail.com>
2019-01-25 02:02:27 +0800

    load without fb instant

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import {
-  Container,
-  Heading,
-  Box,
-  Loader,
-  Notification,
-  Button,
-} from 'react-bulma-components';
+import { Container, Heading, Box } from 'react-bulma-components';
 import { checkStressLevel } from './libs/stress';
-import { startGame } from './libs/fbInstant';
 import StressForm from './containers/StressForm';
 import StressResult from './containers/StressResult';
 
@@ -18,25 +10,9 @@ class App extends Component {
     super(props);
     this.state = {
       showResult: false,
-      isLoading: true,
-      hasError: false,
       stressLevel: 0,
     };
   }
-
-  componentDidMount() {
-    this.handleStart();
-  }
-
-  handleStart = () => {
-    startGame()
-      .then(() => {
-        this.setState({ isLoading: false });
-      })
-      .catch(() => {
-        this.setState({ isLoading: false, hasError: true });
-      });
-  };
 
   handleSubmit = values => {
     this.setState({
@@ -50,22 +26,7 @@ class App extends Component {
   };
 
   render() {
-    const { showResult, stressLevel, isLoading, hasError } = this.state;
-
-    if (isLoading) {
-      return <Loader />;
-    }
-
-    if (hasError) {
-      return (
-        <Box>
-          <Notification color="danger">
-            There was problem starting the game.
-          </Notification>
-          <Button color="primary">Click here to start again</Button>
-        </Box>
-      );
-    }
+    const { showResult, stressLevel } = this.state;
 
     return (
       <Container>
